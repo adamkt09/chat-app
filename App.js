e15cabfe3a17a6c44316a5b810d6a129d5016426
adamkt09 <90269516+adamkt09@users.noreply.github.com>
2022-06-14 10:14:20 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './componets/ChatFeed';
+import LoginForm from './componets/LoginForm';
+import './App.css';
+
+
+const projectID = '2c43c9d8-2a07-414e-b7b1-7b37cc4616c7';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={chatAppProps => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() =>
+        new Audio(
+          'https://chat-engine-assets.s3.amazonaws.com/click.mp3'
+        ).play()
+      }
+    />
+  );
+};
+
+export default App;
